a43af24771303c84da05d66c801149b88ee33467
rqguzman <rafael.guzman@outlook.com>
2018-05-29 12:37:59 -0300

    Created migrations

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
 
-if __name__ == ' __main__ ':
+if __name__ == '__main__':
     app.run()
